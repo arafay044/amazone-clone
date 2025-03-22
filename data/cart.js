80f33fa9071c3f97ce7ab6967e3941189e3a86a3
AbdulRafay <abdul.rafay9714@gmail.com>
2025-03-22 21:41:33 +0500

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];//this variable can be used outside the cart.js file
+//this variable can be used outside the cart.js file
+export const cart = [{
+    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+},{
+    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1
+}];
 
 export function addToCart(productId) {
     let matchingItem;
