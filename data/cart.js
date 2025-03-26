8dd11e321e7a5139b927fa3a2ab1bc0d59d84934
AbdulRafay <abdul.rafay9714@gmail.com>
2025-03-26 10:10:16 +0500

    Create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,8 @@
 //this variable can be used outside the cart.js file
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage();
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
     cart = [{
@@ -12,7 +15,7 @@ if(!cart){
         deliveryOptionId : '2'
     }];
 }
-
+}
 
 
 function saveToStorage(){
