29b9cb50bf826e15b6c1fd32107dced53f930857
AbdulRafay <abdul.rafay9714@gmail.com>
2025-03-29 17:10:38 +0500

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrerSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderOrerSummary();
 renderPaymentSummary();
