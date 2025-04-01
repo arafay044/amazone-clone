14956d28d4578cdca0d3fb898ab010f1814fe070
AbdulRafay <abdul.rafay9714@gmail.com>
2025-04-01 16:21:16 +0500

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -64,3 +64,16 @@ export function addToCart(productId) {
     matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
   }
+
+
+  export function loadCart(func){
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load',() => {
+    console.log(xhr.response);
+    func();
+    });
+    xhr.open('GET','https://supersimplebackend.dev/cart');
+    xhr.send();
+  }
+  
+  
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,9 +2,65 @@ import { renderOrerSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
-import { loadProducts } from "../data/products.js";     
+import { loadProducts } from "../data/products.js";   
+import { loadCart } from "../data/cart.js";
 
-loadProducts(()=>{// callback is a fucntion that we want to run in future
+// PRMOISES: better way to handel asynchronous code (jo wait nai karta complete honay ka)
+//  -- lets us wait for some code to finish, before going to next step
+
+// Built in class and we have to give it a function 
+//  when we create this promise it run this function immediately 
+
+// inner function have resolve parameter just like done in jasmine tell when to go next 
+
+
+// We can run mulitple promises at same time and wait for all of them to finish 
+Promise.all([//takes an array of promises
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');//we can give parameter to resolve function that is saved in then function
+        });
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+
+]).then((values)=>{ //then take a function to run
+    console.log(values);
     renderOrerSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');//we can give parameter to resolve function that is saved in then function
+    });
+
+}).then( (value) => {// promise create a seperate thread for this code and we have to tell next step using then
+    console.log(value);
+
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    });  
+
+}).then(()=>{
+    renderOrerSummary();
+    renderPaymentSummary();
+});
+*/
+
+/*
+// Callback (multiple callback casue alot of nesting => problem)
+loadProducts(()=>{// callback is a fucntion that we want to run in future
+    loadCart(()=>{
+        renderOrerSummary();
+        renderPaymentSummary();
+    });
+    
+});
+*/
